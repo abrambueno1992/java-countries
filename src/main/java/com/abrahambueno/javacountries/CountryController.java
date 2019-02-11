b5cb93f4c493db1f82cdbd5d53d1487787ea0ea5
Abraham Bueno <abueno@localhost.localdomain>
2019-02-11 16:02:45 -0600

    Rout for names/all is now rendering all the countries

--- src/main/java/com/abrahambueno/javacountries/CountryController.java
+++ src/main/java/com/abrahambueno/javacountries/CountryController.java
@@ -1,4 +1,17 @@
 package com.abrahambueno.javacountries;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+// localhost:8080/names/all
+@RestController
+@RequestMapping("/names")
 public class CountryController {
+    @RequestMapping("/all")
+    public ArrayList<Country> getAllCountries() {
+        JavacountriesApplication.ourCountryList.countryList.sort((e1, e2) -> e1.getName().compareToIgnoreCase(e2.getName()));
+        return JavacountriesApplication.ourCountryList.countryList;
+    }
 }
--- src/main/java/com/abrahambueno/javacountries/JavacountriesApplication.java
+++ src/main/java/com/abrahambueno/javacountries/JavacountriesApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavacountriesApplication {
-
+    static CountryList ourCountryList;
     public static void main(String[] args) {
+        ourCountryList = new CountryList();
         SpringApplication.run(JavacountriesApplication.class, args);
     }
 
